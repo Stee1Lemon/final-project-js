227dfbdde64292cc2ea68a5497249d2c174271d5
Olena <shapovalev@gmail.com>
2023-08-18 23:25:21 +0200

    categories/cardMarkUp

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,33 +1,29 @@
 import { FetchInfo } from './fetch-requests';
+import { cardsMarkUp } from './recipes-cards';
 
 const categoriesBtnEl = document.querySelector('.categories-btn-js');
 const categoriesListEl = document.querySelector('.categories-list-js');
 
 let categoryBtns = [];
 
-function getCategories() {
+async function getCategories() {
   const categories = new FetchInfo();
-  categories
-    .fetchAllCategories()
-    .then(resp => {
-      categoriesListEl.insertAdjacentHTML(
-        'afterbegin',
-        createCategoriesMarkUp(resp.data)
-      );
-      const btns = document.querySelectorAll('.category-btn');
-      categoryBtns = [...btns];
-    })
-    .catch(err => {
-      console.log(err);
-      categoriesListEl.insertAdjacentHTML(
-        'afterbegin',
-        `
-            <p class="categories-err">
-            We are sorry, something went wrong. Please, reload the page!
-            </p>
-            `
-      );
-    });
+  try {
+    const resp = await categories.fetchAllCategories();
+    categoriesListEl.insertAdjacentHTML(
+      'afterbegin',
+      createCategoriesMarkUp(resp.data)
+    );
+    const btns = document.querySelectorAll('.category-btn');
+    categoryBtns = [...btns];
+  } catch (err) {
+    console.log(err);
+    categoriesListEl.innerHTML = `
+                <p class="categories-err">
+                We are sorry, something went wrong. Please, reload the page!
+                </p>
+                `;
+  }
 }
 
 getCategories();
@@ -47,17 +43,19 @@ function createCategoriesMarkUp(arr) {
 categoriesBtnEl.addEventListener('click', handlerAllCategoriesBtn);
 
 function handlerAllCategoriesBtn() {
-  getAllRecipes();
   makeBtnNotActive();
+  getAllRecipes();
 }
 
-function getAllRecipes() {
-    const allRecipes = new FetchInfo();
-    allRecipes.fetchAllRecipesPerPage(limit = 9)
-    .then(resp => {
-      //   функ що приймає масив resp.data.results і рендерить всі рецепти (innerHTML)
-    })
-    .catch(err => console.log(err));
+async function getAllRecipes() {
+  const allRecipes = new FetchInfo();
+  try {
+    const resp = await allRecipes.fetchAllRecipesPerPage((limit = 9));
+    cardsMarkUp(resp.data.results);
+  } catch (err) {
+    console.log(err);
+    //   функ що малює помилку при рендері всіх рецептів на місці рецептів і приймає рядок повідомлення ('We are sorry. Something went wrong. Please, try reload the page.')
+  }
   //   функ що малює помилку при рендері всіх рецептів на місці рецептів і приймає рядок повідомлення ('We are sorry. Something went wrong. Please, try reload the page.')
 }
 
@@ -67,27 +65,31 @@ function handlerCategoryBtn(ev) {
   if (ev.target.nodeName !== 'BUTTON') {
     return;
   }
-  makeBtnNotActive();
-
   if (ev.target.classList.contains('category-btn-active')) {
     return;
   }
+  makeBtnNotActive();
   ev.target.classList.add('category-btn-active');
   const nameOfCategory = ev.target.textContent;
   getRecipesByCategory(nameOfCategory);
 }
 
-function getRecipesByCategory(category) {
-    const recipesByCategory = new FetchInfo();
-    recipesByCategory.fetchByCategory(category, page = 1, limit = 9)
-    .then(resp => {
-      if (resp.data.results.length === 0) {
-        // функ що малює помилку при рендері рецептів рецептів по категорії на місці рецептів і приймає рядок повідомлення ('We are sorry. There are no recipes in this category.');
-      }
-      //   функ що приймає масив resp.data.results і рендерить картки по категоріі with innerHTML
-    })
-    .catch(err => console.log(err));
-  //   функ що малює помилку при рендері рецептів по категорії на місці рецептів і приймає рядок повідомлення ('We are sorry. Something went wrong. Please, try reload the page.')
+async function getRecipesByCategory(category) {
+  const recipesByCategory = new FetchInfo();
+  try {
+    const resp = await recipesByCategory.fetchByCategory(
+      category,
+      (page = 1),
+      (limit = 9)
+    );
+    if (resp.data.results.length === 0) {
+      // функ що малює помилку при рендері рецептів рецептів по категорії на місці рецептів і приймає рядок повідомлення ('We are sorry. There are no recipes in this category.');
+    }
+    cardsMarkUp(resp.data.results);
+  } catch (err) {
+    console.log(err);
+    //   функ що малює помилку при рендері рецептів по категорії на місці рецептів і приймає рядок повідомлення ('We are sorry. Something went wrong. Please, try reload the page.')
+  }
 }
 
 function makeBtnNotActive() {
@@ -97,3 +99,11 @@ function makeBtnNotActive() {
     }
   });
 }
+
+export function getNameOfActiveCategory() {
+    categoryBtns.map(btn => {
+      if (btn.classList.contains('category-btn-active')) {
+        return btn.textContent
+      }
+    });
+  } 
