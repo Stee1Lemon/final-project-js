5643eef0211ecde92c192662b12e4eb74d3b919a
Katrinnk <katerin69llobster@gmail.com>
2023-08-19 15:26:52 +0300

    p1

--- src/js/recipe-pop-up.js
+++ src/js/recipe-pop-up.js
@@ -1,1 +1,30 @@
-// console.log('9. Recipe pop up')
+import { all } from "axios";
+import { FetchInfo } from "./fetch-requests";
+
+const recipes = new FetchInfo();
+
+
+const recipeContainer = document.querySelector('.modal-recipe-content');
+
+
+recipes.fetchRecipeById()
+.then(data => {
+    console.log(data);
+    recipeMarkup(data);
+});
+
+
+
+function recipeMarkup(resipeData){
+    const {youtube, title, rating, time, ingredients, tags, instructions} = resipeData; 
+    console.log(resipeData);
+    const recipeMarkup = `<iframe src="${youtube}" width="295" height="295"></iframe>
+    <h2>${title}</h2>
+    <p>${rating}</p>
+    <p>${time} min</p>
+    <p>${ingredients}</p>
+    <p>${tags}</p>
+    <p>${instructions}</p>
+    `;
+    recipeContainer.innerHTML = recipeMarkup;
+}
