ee83d2498143e36729ad768767491b4bd4917255
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-08-20 18:28:50 +0100

    Update rating-pop-up-real.js

--- src/js/rating-pop-up-real.js
+++ src/js/rating-pop-up-real.js
@@ -2,8 +2,8 @@ const ratingOpenBtn = document.querySelector('.open-rating-btn');
 const ratingPopUpEl = document.querySelector('.container-rating');
 const ratingCloseBtn = document.querySelector('.rating-btn-close');
 
-ratingOpenBtn.addEventListener('click', toggleRatingPopUp);
-ratingCloseBtn.addEventListener('click', toggleRatingPopUp);
+ratingOpenBtn?.addEventListener('click', toggleRatingPopUp);
+ratingCloseBtn?.addEventListener('click', toggleRatingPopUp);
 
 function toggleRatingPopUp() {
   ratingPopUpEl.classList.toggle('is-hidden');
