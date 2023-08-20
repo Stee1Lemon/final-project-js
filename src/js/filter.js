49ba4e8cc835c67c290a63d74fad19cc8aef7de2
Movash <126423674+Movash@users.noreply.github.com>
2023-08-21 00:31:05 +0300

    export_filters

--- src/js/filter.js
+++ src/js/filter.js
@@ -8,26 +8,36 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { FetchInfo } from './fetch-requests.js';
 
+const filters = new FetchInfo();
+
 const selectArea = document.querySelector('.area-select');
 const selectIngredients = document.querySelector('.ingredients-select');
 const selectTime = document.querySelector('.time-select');
+const inputSubmit = document.querySelector('.search-input');
 
-selectArea.addEventListener('change', selectedArea)
-selectIngredients.addEventListener('change', selectedIngredient);
+selectArea?.addEventListener('change', selectedArea);
+selectIngredients?.addEventListener('change', selectedIngredient);
+selectTime?.addEventListener('change', selectedTime);
+inputSubmit?.addEventListener('input', debounce(handlerInput, 300));
 
-function selectedArea(evt) {
-  searchArea = evt.currentTarget.value
-  console.log(searchArea);
-}
+let searchArea = '';
+let searchIngredient = ''; 
+let searchTime = ''; 
+let searchText = '';
 
-function selectedIngredient(evt) {
-  searchIngredient = evt.currentTarget.value;
-  console.log(searchIngredient);
-}
+export const fullFilter = {
+  search: searchText,
+  time: searchTime,
+  area: searchArea,
+  ingredients: searchIngredient,
+};
+
+const defaultTimeOption = 
+  '<option class="" value="" selected>0 min</option>';
 
 function timesMarkup() {
   let secondsStep = [];
-  for (let minutes = 0; minutes <= 120; minutes += 5) {
+  for (let minutes = 5; minutes <= 120; minutes += 5) {
     secondsStep.push(`${minutes}`);
   }
   let timeMarkup = secondsStep
@@ -35,47 +45,47 @@ function timesMarkup() {
       return `<option value="${minute}">${minute} min</option>`;
     })
     .join('');
-  selectTime.insertAdjacentHTML('beforeend', timeMarkup);
+    const selectTimeMarkup = defaultTimeOption + timeMarkup;
+  selectTime.insertAdjacentHTML('beforeend', selectTimeMarkup);
 
   new SlimSelect({
     select: selectTime,
   });
 }
+
 timesMarkup();
 
-const filters = new FetchInfo();
-
 const defaultAreaOption =
-  '<option value="value" selected>Region</option>';
+  '<option class="" value="" selected>Region</option>';
 
 filters
   .fetchAllAreas()
   .then(resp => {
     const areasMarkup = resp.data
       .map(area => {
-        return `<option value="${area._id}">${area.name}</option>`;
+        return `<option class="sisi dodo" value="${area.name}">${area.name}</option>`;
       })
       .join('');
       const selectAreaMarkup = defaultAreaOption + areasMarkup;
-    selectArea.insertAdjacentHTML('beforeend', selectAreaMarkup);
-
-    new SlimSelect({
-      select: selectArea,
-    });
+      selectArea.insertAdjacentHTML('beforeend', selectAreaMarkup);
+      
+      new SlimSelect({
+        select: selectArea,
+      });
   })
   .catch(() => {
     Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
   });
 
   const defaultIngredientsOption =
-    '<option value="value" selected>Product</option>';
+    '<option value="" selected>Product</option>';
 
 filters
   .fetchAllIngredients()
   .then(resp => {
     const ingredientsMarkup = resp.data
       .map(ingredient => {
-        return `<option value="${ingredient._id}">${ingredient.name}</option>`;
+        return `<option value="${ingredient.name}">${ingredient.name}</option>`;
       })
       .join('');
       const selectIngredientsMarkup =
@@ -90,18 +100,30 @@ filters
     Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
   });
 
-const inputSubmit = document.querySelector('.search-input');
-
-inputSubmit.addEventListener('input', debounce(handlerInput, 300));
-
 function handlerInput(evt) {
   searchText = evt.target.value.trim().toLowerCase();
-  console.log(searchText);
-  
-  // if (searchText === '') {
-  //   виклик функціх з All categories
-  //   return
-  // }
+  updateFullFilter();
 }
 
-// function foo (searchText, )
+function selectedTime(evt) {
+  searchTime = evt.currentTarget.value;
+  updateFullFilter();
+}
+
+function selectedArea(evt) {
+  searchArea = evt.currentTarget.value;
+  updateFullFilter();
+}
+
+function selectedIngredient(evt) {
+  searchIngredient = evt.currentTarget.value;
+  updateFullFilter();
+}
+
+function updateFullFilter() {
+  fullFilter.search = searchText;
+  fullFilter.time = searchTime;
+  fullFilter.area = searchArea;
+  fullFilter.ingredients = searchIngredient;
+  console.log(fullFilter);
+}
