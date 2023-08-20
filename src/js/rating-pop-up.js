6c046883f4ed8cba87941bfef481fc6219848fa1
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-08-20 14:33:08 +0300

    Added code for modals

--- src/js/rating-pop-up.js
+++ src/js/rating-pop-up.js
@@ -1,1 +1,41 @@
-// console.log('8. Rating pop up');
+import * as basicLightbox from 'basiclightbox';
+
+export function createModal(htmlContent) {
+  const instance = basicLightbox.create(htmlContent);
+
+  function closeModal() {
+    instance.close();
+    removeEventListeners();
+  }
+
+  function addEventListeners() {
+    instance.element().addEventListener('click', (e) => {
+      if (e.target === instance.element()) {
+        closeModal();
+      }
+    });
+
+    const closeButton = instance.element().querySelector('.close-button');
+    if (closeButton) {
+      closeButton.addEventListener('click', closeModal);
+    }
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    });
+  }
+
+  function removeEventListeners() {
+    instance.element().removeEventListener('click', closeModal);
+    const closeButton = instance.element().querySelector('.close-button');
+    if (closeButton) {
+      closeButton.removeEventListener('click', closeModal);
+    }
+    document.removeEventListener('keydown', closeModal);
+  }
+
+  instance.show();
+  addEventListeners();
+}
