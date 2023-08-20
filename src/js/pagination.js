838025792ab736b523af4b71c8cb6924ea649e46
RomanProtsenko <126424199+RomanProtsenko@users.noreply.github.com>
2023-08-20 20:55:49 +0300

    7

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,11 +1,9 @@
 import Pagination from 'tui-pagination';
 
-
 const container = document.getElementById('pagination');
 
-
 const options = {
-  totalItems: 0,
+  totalItems: 100, // Припустимо, що у вас є 100 елементів, які потрібно розподілити на сторінки
   itemsPerPage: 9,
   visiblePages: window.innerWidth < 768 ? 2 : 3,
   page: 1,
@@ -31,5 +29,18 @@ const options = {
 };
 
 const pagination = new Pagination(container, options);
+
+const pageButtons = container.querySelectorAll('.tui-page-btn');
+pageButtons.forEach((button, index) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault(); 
+      const clickedPage = index + 1;
+      console.log(`Користувач клікнув на сторінку ${clickedPage}`);
+      });
+});
+
+function someFn(pageNumber) {
+  console.log(`Обраний номер сторінки: ${pageNumber}`);
+}
 
 export { pagination };
