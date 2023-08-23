fe5c7962ccde16bbfa8718e014dbee857e4b7d91
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-08-23 19:54:49 +0300

    Fixed images

--- src/js/developer-modal.js
+++ src/js/developer-modal.js
@@ -1,4 +1,6 @@
 import { createModal } from './open-any-modal.js';
+import manImage from '../images/images-developer/man-image.webp';
+import womanImage from '../images/images-developer/woman-image.webp';
 
 const developerOpenBtn = document.querySelector('.team-section');
 
@@ -11,52 +13,52 @@ function openTeamModal() {
 function getModalContent() {
   return `<div class="developer-list">
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Vladyslav Holodniuk</span><br>
       <span class="developer-role">Team Lead</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/woman-image.webp">
+      <img class="footer-modal-image" src="${womanImage}">
       <span class="developer-name">Olena Kondrachuk</span><br>
       <span class="developer-role">Scrum Master</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Oleksandr Andrushchenko</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/woman-image.webp">
+      <img class="footer-modal-image" src="${womanImage}">
       <span class="developer-name">Kateryna Kalmykova</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/woman-image.webp">
+      <img class="footer-modal-image" src="${womanImage}">
       <span class="developer-name">Karyna Pavliuk</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Andrii Kozyr</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Viacheslav Sosiedko</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Andrii Kharchenko</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/woman-image.webp">
+      <img class="footer-modal-image" src="${womanImage}">
       <span class="developer-name">Anna Turchenko</span>
       <span class="developer-role">Developer</span>
   </div>
   <div class="developer-list-item">
-      <img class="footer-modal-image" src="/images/images-developer/man-image.webp">
+      <img class="footer-modal-image" src="${manImage}">
       <span class="developer-name">Roman Protsenko</span>
       <span class="developer-role">Developer</span>
   </div>
